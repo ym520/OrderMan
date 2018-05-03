9a722c8ed70c291c3245e9857e2695883a95b38c
yangming <yangming@qq.com>
2018-05-03 15:34:14 +0800

    新增文件

---
+++ app/src/main/java/com/colorlife/orderman/Activity/cook/CookManager.java
@@ -0,0 +1,31 @@
+package com.colorlife.orderman.Activity.cook;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.colorlife.orderman.Activity.base.ActivityCollector;
+import com.colorlife.orderman.R;
+
+import org.xutils.view.annotation.ContentView;
+import org.xutils.x;
+
+/**
+ * Created by ym on 2018/5/3 0003.
+ */
+@ContentView(R.layout.cook_manager)
+public class CookManager extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        x.view().inject(this);
+        ActivityCollector.addActivity(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        ActivityCollector.removeActivity(this);
+    }
+}
---
+++ app/src/main/java/com/colorlife/orderman/Activity/setting/settingIndex.java
@@ -0,0 +1,16 @@
+package com.colorlife.orderman.Activity.setting;
+
+import com.colorlife.orderman.Activity.base.BaseActivity;
+import com.colorlife.orderman.R;
+
+import org.xutils.view.annotation.ContentView;
+import org.xutils.view.annotation.ViewInject;
+
+/**
+ * Created by ym on 2018/5/3 0003.
+ */
+
+@ContentView(R.layout.setting_index)
+public class settingIndex extends BaseActivity {
+
+}
