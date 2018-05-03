3c5a47c82aea725e68fe27ef9a18a1785b012d94
杨明 <>
2018-05-03 20:54:15 +0800

    新增文件

--- app/src/main/java/com/colorlife/orderman/Activity/setting/settingIndex.java
+++ app/src/main/java/com/colorlife/orderman/Activity/setting/settingIndex.java
@@ -1,9 +1,15 @@
 package com.colorlife.orderman.Activity.setting;
 
+import android.content.Intent;
+import android.view.View;
+import android.widget.LinearLayout;
+
 import com.colorlife.orderman.Activity.base.BaseActivity;
+import com.colorlife.orderman.Activity.cook.CookManager;
 import com.colorlife.orderman.R;
 
 import org.xutils.view.annotation.ContentView;
+import org.xutils.view.annotation.Event;
 import org.xutils.view.annotation.ViewInject;
 
 /**
@@ -12,5 +18,30 @@ import org.xutils.view.annotation.ViewInject;
 
 @ContentView(R.layout.setting_index)
 public class settingIndex extends BaseActivity {
+
+    @ViewInject(R.id.setting_LinearLayout_cashSetting)
+    private LinearLayout cash;
+    @ViewInject(R.id.setting_LinearLayout_cookManager)
+    private LinearLayout cookManager;
+    @ViewInject(R.id.setting_LinearLayout_deskManager)
+    private LinearLayout deskManager;
+    @ViewInject(R.id.setting_LinearLayout_printSetting)
+    private LinearLayout printSetting;
+    @ViewInject(R.id.setting_LinearLayout_userSetting)
+    private LinearLayout userSetting;
+    @ViewInject(R.id.setting_LinearLayout_version)
+    private LinearLayout version;
+
+    @Event(R.id.setting_LinearLayout_deskManager)
+    private void intoDeskManager(){
+        Intent intent=new Intent(settingIndex.this,DeskManager.class);
+        startActivity(intent);
+    }
+
+    @Event(R.id.setting_LinearLayout_cookManager)
+    private void intoCookManager(View view){
+        Intent intent=new Intent(settingIndex.this, CookManager.class);
+        startActivity(intent);
+    }
 
 }
